130d0cf11d5cb16994ad455df14f03cb21fee109
Tino <Tino@LAPTOP-861SQQ0B>
2021-03-20 11:43:08 -0600

    Revert "Cuarto cambio Tino"
    This reverts commit 567e85d2174c962d4d05a5bfbbbefe4058a16769.

--- src/proyectotp/FXMLDocumentController.java
+++ src/proyectotp/FXMLDocumentController.java
@@ -5,8 +5,6 @@
  */
 package proyectotp;
 
-import java.awt.Toolkit;
-import java.awt.event.KeyEvent;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -16,7 +14,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 
 /**
@@ -27,32 +24,23 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private Label label;
-    
-    @FXML
-    private Slider horas_Laboradas;
 
     @FXML
     private ComboBox<String> combo_Empleado;
     
-    @FXML 
-    private TextField nombre_Empleado, salario_Empleado;
-    
-    
+    //@FXML 
+    //private TextField<String> nombre_Empleado, salario_Empleado;
+
     
     @FXML
     private void comboboxEvent(ActionEvent event) {
         
     }
     
-   
     @FXML
     private void handleButtonAction(ActionEvent event)
     {
-        System.out.println("Calculando salario....");
-        System.out.println(nombre_Empleado.getText());
-        System.out.println(salario_Empleado.getText());
-        System.out.println(combo_Empleado.getValue());
-        System.out.println(horas_Laboradas.getValue());
+    
     }
     
     
@@ -62,7 +50,7 @@ public class FXMLDocumentController implements Initializable {
         ArrayList <String> empleados = new ArrayList<>();
         Collections.addAll(empleados,new String[]{"Gerente","Coordinador","Empleado general"});
         combo_Empleado.getItems().addAll(empleados);
-        
+
     }    
     
 }
